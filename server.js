7f58798227c12d3f8d240453f333163009ccf625
CarL Liu <thecarlliu99@er-10-132-150-170.wireless.umn.edu>
2018-02-24 11:27:42 -0600

    Added server routesand array of objects for table data

--- server.js
+++ server.js
@@ -8,6 +8,36 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var reservations = [
+    {
+        routeName: "reservation1",
+        name: "Carl",
+        persons: "5",
+        date: "2/25/18",
+        time: "8pm"
+    }
+];
+
+var waitList = [
+    {
+        routeName: "wait1",
+        name: "Derek",
+        persons: "6",
+        date: "2/28/18",
+        time: "7pm"
+    }
+];
+
+app.get("/", function(req, res) {
+    //res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+    //res.sendFile(path.join(__dirname, "tables.html"));
+    res.json(reservations);
+    res.json(waitList);
+});
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
